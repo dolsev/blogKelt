818ab33dc96a909ad122b699fbbadf76b3efab0f
dolsev <dolod777@yandex.ru>
2022-08-16 10:26:58 +0300

    Almost finalized version

--- app.js
+++ app.js
@@ -9,14 +9,14 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
-
+const posts =[];
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get ('/',(req,res)=>{
-  res.render ('home',{startingContent:homeStartingContent})
+  res.render ('home',{startingContent:homeStartingContent,posts:posts});
 })
 
 app.get ('/about',(req,res)=>{
@@ -32,17 +32,11 @@ app.get ('/compose',(req,res)=>{
 })
 
 app.post ('/compose',(req,res)=>{
-  console.log(req.body.textInput);
-  res.redirect('/compose'); 
+  let postedo =req.body;
+  posts.push(postedo);
+  res.redirect('/')
 
 })
-
-
-
-
-
-
-
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
